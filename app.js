d9f7defbfbd6db673b050e548606c674f11871af
playscodes <38404830+playscodes@users.noreply.github.com>
2018-05-29 17:02:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({secret: 'SOIEF8923kiwf'}))
+app.use(session({secret: ''}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
